aed5f78bf6336a83f891726c2f0025a9a602fcf5
tonnon <lucastonnon@gmail.com>
2024-08-26 12:56:29 -0300

    project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas } from 'react-three-fiber';
+import circleImg from './assets/circle.png';
+import { Suspense } from 'react';
+
+function Points() { }
+
+function AnimationCanvas() {
+  return (
+    <Canvas
+      colorManagement={false}
+      camera={{ position: [100, 10, 0], fov: 75 }}
+    >
+      <Points />
+    </Canvas>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="anim">
+      <Suspense fallback={<div> Loading... </div>}>
+        <AnimationCanvas />
+      </Suspense>
     </div>
   );
 }
